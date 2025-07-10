e65069f6af0545d0cddef0b806d21d6161f8360a
vanshika sahu <vanshikasahu281105@gmail.com>
2025-07-10 23:21:59 +0530

    Add files via upload

---
+++ Assignments/55/55.2/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
+import ProductsDetails from './Components/ProductsDetails'
+
+function App() {
+
+  return (
+    <>
+    {/* <Header />
+    <Main />
+    <Footer /> */}
+   <ProductsDetails />
+    </>
+  )
+}
+
+export default App
