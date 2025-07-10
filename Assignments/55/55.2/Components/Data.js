d8090eeb2b28a4eb854d2d6cae5a1aadfd5629f9
vanshika sahu <vanshikasahu281105@gmail.com>
2025-07-10 23:24:22 +0530

    Add files via upload

---
+++ Assignments/55/55.2/Components/Data.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    img: "https://plus.unsplash.com/premium_photo-1718913936342-eaafff98834b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d2hpdGUlMjB0JTIwc2hpcnR8ZW58MHx8MHx8fDA%3D",
+    category: "T-Shirt",
+    title: "White Plain T-Shirt",
+    price: "$15.00",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    sale: false,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1620799139507-2a76f79a2f4d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdoaXRlJTIwdCUyMHNoaXJ0fGVufDB8fDB8fHww",
+    category: "T-Shirt",
+    title: "Black Plain T-Shirt",
+    price: "$20.00",
+    sale: false,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2hpcnR8ZW58MHx8MHx8fDA%3D",
+    category: "T-Shirt",
+    title: "Black Plain T-Shirt",
+    price: "$20.00",
+    sale: false,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1719289799337-9cb436447965?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bXVnfGVufDB8fDB8fHww",
+    category: "Mug",
+    title: "White Mug",
+    price: "$5.00",
+    sale: false,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1673422506808-ad558246598a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fG11Z3xlbnwwfHwwfHx8MA%3D%3D",
+    category: "Tea Cup",
+    title: "Chocolate Tea Cup",
+    price: "$5.00",
+    sale: true,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1525972757199-cf2ad7cc4f4b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y3Vwc3xlbnwwfHwwfHx8MA%3D%3D",
+    category: "Cup",
+    title: "Whites Tea Cup",
+    price: "$4.00",
+    sale: false,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+    category: "Full T-Shirt",
+    title: "White Plain full T-Shirt",
+    price: "$25.00",
+    sale: true,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1595434091143-b375ced5fe5c?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGN1cHxlbnwwfHwwfHx8MA%3D%3D",
+    category: "Cups",
+    title: "white Tea Cup",
+    price: "$10.00",
+    sale: true,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    img: "https://media.istockphoto.com/id/1301050867/photo/handsome-attractive-european-muscular-man-in-orange-hoodie.webp?a=1&b=1&s=612x612&w=0&k=20&c=TDz9-YHoMvJ8j6vcKSL0ZuOLMPYbb-7HPSWpNkURoOg=",
+    category: "Hood",
+    title: "Yellow hood",
+    price: "$20.00",
+    sale: true,
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
---
+++ Assignments/55/55.2/Components/Main.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Product from "./Product";
+import Data from "./Data";
+import Button from "./Button";
+const Main = () => {
+  return (
+    <div className="bg-gray-100 p-10">
+      <div className="bg-white shadow-md w-[80vw] mx-auto p-10 flex flex-col gap-6">
+        <div className="text-right">
+          <select
+            className="border-1 bg-gray-50 px-5 py-1 outline-0"
+            name=""
+            id="dropDown"
+          >
+            <option className="hidden" value="Default Sort">
+              Default Sort
+            </option>
+            <option value="SortByClothes">SortByClothes</option>
+            <option value="SortByCups">SortByCups</option>
+          </select>
+        </div>
+        <div className="flex flex-wrap justify-between ">
+          {Data.map((product) => (
+            <Product
+              img={product.img}
+              category={product.category}
+              title={product.title}
+              price={product.price}
+              sale={product.sale}
+            />
+          ))}
+        </div>
+        <div>
+          <Button />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
